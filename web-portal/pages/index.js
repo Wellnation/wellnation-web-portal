3e41a383f61caa32997052467e8908dbdcddc407
DarthSalad <piyushmishra965@gmail.com>
2023-03-30 10:31:39 +0530

    index.js data grid

--- web-portal/pages/index.js
+++ web-portal/pages/index.js
@@ -1,47 +1,119 @@
+import React from 'react';
+import { db } from '@/lib/firebase.config';
+import { collection, query, getDocs, doc as firestoreDoc, getDoc, where } from "firebase/firestore";
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
+import { useQuery } from 'react-query';
+import { useAuthStore } from '@/lib/zustand.config';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
+  { 
+    field: 'id', 
+    headerName: 'ID', 
+    width: 90 
+  },
   {
-    field: 'equipment',
-    headerName: 'Equipment',
+    field: 'patient',
+    headerName: 'Patient Name',
+    type: 'string',
     width: 150,
-    editable: true,
   },
   {
-    field: 'amount',
-    headerName: 'Amount',
-    type: 'number',
+    field: 'doctorName',
+    headerName: 'Doctor Name',
+    type: 'string',
+    width: 150,
+  },
+  {
+    field: 'hospitalName',
+    headerName: 'Hospital Name',
+    type: 'string',
     width: 110,
-    editable: true,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    field: 'cause',
+    headerName: 'Cause',
+    type: 'string',
+    width: 110,
   },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  {
+    field: 'time',
+    headerName: 'Date',
+    type: 'date',
+    width: 110,
+    valueGetter: (params) => {
+      return params.row.time.toDate();
+    },
+  },
+  {
+    field: 'completed',
+    headerName: 'Status',
+    type: 'string',
+    width: 110,
+    // editable: true,
+  },
 ];
 
 const Home = () => {
+  const { user, loading, userError } = useAuthStore();
+  // const { data: appointments, isLoading, error } = useQuery('appointments', async () => {
+  //   const querySnap = await getDocs(collection(db, 'users'), where('email', '==', user.email));
+  //   const hospitalId = querySnap.docs[1].id;
+  //   const querySnapshot = await getDocs(query(collection(db, 'appointments'), where('hospital', '==', hospitalId)));
+  //   const appointmentsData = await Promise.all(
+  //     querySnapshot.docs.map(async (doc) => {
+  //       const appointmentData = doc.data();
+  //       const userDoc = await getDoc(firestoreDoc(db, 'users', appointmentData.hospital));
+  //       const doctorName = userDoc.data().doctors.find((doctor) => doctor.id === appointmentData.doctor).name;
+  //       return { id: doc.id, ...appointmentData, hospitalName: userDoc.data().name, doctorName: doctorName };
+  //     })
+  //   );
+  //   return appointmentsData;
+  // });
+  const [appointmentData, setAppointmentData] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    const getAppointmentData = async () => {
+      try {
+        const querySnap = await getDocs(collection(db, 'users'), where('email', '==', user.email));
+        const hospitalId = querySnap.docs[1].id;
+        const querySnapshot = await getDocs(query(collection(db, 'appointments'), where('hospital', '==', hospitalId)));
+        const appointmentsData = await Promise.all(
+          querySnapshot.docs.map(async (doc) => {
+            const appointmentData = doc.data();
+            const userDoc = await getDoc(firestoreDoc(db, 'users', appointmentData.hospital));
+            const doctorName = userDoc.data().doctors.find((doctor) => doctor.id === appointmentData.doctor).name;
+            return { id: doc.id, ...appointmentData, hospitalName: userDoc.data().name, doctorName: doctorName };
+          })
+        );
+        setAppointmentData(appointmentsData);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    }
+
+    getAppointmentData();
+  }, [user.email]);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) { 
+    return (
+      <div>
+        An error occurred: {error.message}<br />
+        {user.email}
+      </div>
+    );
+  }
+
   return (
     <>
+      <h1>Appointments</h1>
+      {user.email}
       <div 
         style={{
           display: 'flex',
@@ -52,7 +124,7 @@ const Home = () => {
       >
         <Box sx={{ height: 400, width: '80%' }}>
           <DataGrid
-            rows={rows}
+            rows={appointmentData}
             columns={columns}
             initialState={{
               pagination: {
@@ -62,7 +134,7 @@ const Home = () => {
               },
             }}
             pageSizeOptions={[5]}
-            checkboxSelection
+            // checkboxSelection
             disableRowSelectionOnClick
           />
         </Box>
