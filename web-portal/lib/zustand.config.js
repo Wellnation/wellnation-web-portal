bbbf5f5fd6dda9562af1963b3e4833f97fae8d68
DarthSalad <piyushmishra965@gmail.com>
2023-03-30 16:51:26 +0530

    sample message

--- web-portal/lib/zustand.config.js
+++ web-portal/lib/zustand.config.js
@@ -1,16 +1,32 @@
 import { create } from 'zustand';
 import { auth } from './firebase.config';
 import { onAuthStateChanged } from 'firebase/auth';
+import { useQuery } from 'react-query';
 
-const useAuthStore = create((set) => ({
+const useStore = create((set) => ({
   user: null,
   loading: true,
   setUser: (user) => set({ user }),
   setLoading: (loading) => set({ loading })
 }));
 
+function useAuth() {
+  const setUser = useStore(state => state.setUser);
+
+  useQuery('auth', () => new Promise(resolve => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setUser(user);
+      setLoading(false);
+      resolve(user);
+    });
+    return unsubscribe;
+  }));
+
+  return useStore(state => state.user);
+}
+
 onAuthStateChanged(auth, (user) => {
   useAuthStore.setState({ user, loading: false });
 });
 
-export { useAuthStore };
+export { useAuth };
