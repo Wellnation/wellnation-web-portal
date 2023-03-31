11c16fd16cea177cb00d5b9fc61b7a78680986fc
oyesaurav <78659500+oyesaurav@users.noreply.github.com>
2023-03-31 08:49:09 +0530

    test historu usequery

--- web-portal/pages/history.js
+++ web-portal/pages/history.js
@@ -1,49 +1,45 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import TablePagination from '@mui/material/TablePagination';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import { useQuery } from 'react-query';
-import { collection, getDocs, where, query } from 'firebase/firestore';
-import { db } from '@/lib/firebase.config';
-import { useAuth } from '@/lib/zustand.config';
-``
+import * as React from "react"
+import PropTypes from "prop-types"
+import Box from "@mui/material/Box"
+import Collapse from "@mui/material/Collapse"
+import IconButton from "@mui/material/IconButton"
+import Table from "@mui/material/Table"
+import TableBody from "@mui/material/TableBody"
+import TableCell from "@mui/material/TableCell"
+import TableContainer from "@mui/material/TableContainer"
+import TableHead from "@mui/material/TableHead"
+import TableRow from "@mui/material/TableRow"
+import Typography from "@mui/material/Typography"
+import Paper from "@mui/material/Paper"
+import TablePagination from "@mui/material/TablePagination"
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown"
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp"
+import { useQuery } from "react-query"
+import { collection, getDocs, where, query, getDoc } from "firebase/firestore"
+import { db } from "@/lib/firebase.config"
+import { useAuth } from "@/lib/zustand.config"
+import { NotUser, Loader } from "@/components/utils"
+;``
 function Row(props) {
-  const { row } = props;
-  const [open, setOpen] = React.useState(false);
+  const { row } = props
+  const [open, setOpen] = React.useState(false)
 
   return (
     <React.Fragment>
-      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+      <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
         <TableCell>
-          <IconButton
-            aria-label="expand row"
-            size="small"
-            onClick={() => setOpen(!open)}
-          >
+          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
         </TableCell>
         <TableCell component="th" scope="row">
-          {row.name}
+          {row.PatientName}
         </TableCell>
-        <TableCell align="right">{row.PatientName}</TableCell>
-        <TableCell align="right">{row.Date}</TableCell>
-        <TableCell align="right">{row.Time}</TableCell>
-        <TableCell align="right">{row.Tests}</TableCell>
+        <TableCell align="right">{row.date.toDate().toDateString()}</TableCell>
+        <TableCell align="right">{row.date.toDate().toLocaleTimeString('en-us')}</TableCell>
+        <TableCell align="right">{row.type}</TableCell>
       </TableRow>
-      <TableRow>
+      {/* <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box sx={{ margin: 1 }}>
@@ -77,132 +73,126 @@ function Row(props) {
             </Box>
           </Collapse>
         </TableCell>
-      </TableRow>
+      </TableRow> */}
     </React.Fragment>
-  );
+  )
 }
 
-Row.propTypes = {
-  row: PropTypes.shape({
-    PatientName: PropTypes.string.isRequired,
-    Date: PropTypes.string.isRequired,
-    Time: PropTypes.string.isRequired,
+// Row.propTypes = {
+//   row: PropTypes.shape({
+//     PatientName: PropTypes.string.isRequired,
+//     date: PropTypes.string.isRequired,
 
-    Tests: PropTypes.arrayOf(
-      PropTypes.shape({
-        type: PropTypes.number.isRequired,
-      }),
-    ).isRequired,
-    history: PropTypes.arrayOf(
-      PropTypes.shape({
-        Name: PropTypes.string.isRequired,
-        customerId: PropTypes.string.isRequired,
-        date: PropTypes.string.isRequired,
-      }),
-    ).isRequired
-  }).isRequired,
-};
+//     type: PropTypes.arrayOf(
+//       PropTypes.shape({
+//         type: PropTypes.number.isRequired,
+//       })
+//     ).isRequired,
+//   }).isRequired,
+// }
 
 const columns = [
-  { id: 'patientHist', label: 'Patient history', minWidth: 10 },
-  { id: 'patient', label: 'Patient name', minWidth: 10 },
+  { id: "patientHist", label: "Patient history", minWidth: 10 },
+  { id: "patient", label: "Patient name", minWidth: 10 },
   {
-    id: 'Date',
-    label: 'Date',
+    id: "Date",
+    label: "Date",
     minWidth: 100,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
+    align: "right",
+    format: (value) => value.toLocaleString("en-US"),
   },
   {
-    id: 'Time',
-    label: 'Time',
+    id: "Time",
+    label: "Time",
     minWidth: 100,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
+    align: "right",
+    format: (value) => value.toLocaleString("en-US"),
   },
   {
-    id: 'Tests',
-    label: 'Tests',
+    id: "Tests",
+    label: "Tests",
     minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
+    align: "right",
+    format: (value) => value.toLocaleString("en-US"),
   },
-];
-
-const rows = []
+]
 
 export default function History() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [page, setPage] = React.useState(0)
+  const [rowsPerPage, setRowsPerPage] = React.useState(10)
+  const hId = localStorage.getItem("hId")
 
-  const { user, loading, userError } = useAuth();
-
+  const { user, loading, userError } = useAuth()
   const { isLoading, error, data } = useQuery({
-    queryKey: ['tests'],
+    queryKey: ["tests"],
     queryFn: fetchtests,
-  });
+  })
 
   async function fetchtests() {
-    // console.log(user)
-    const hDoc = await getDocs(query(collection(db, "users"), where("email", "==", user.email)));
-    console.log(hDoc.docs[0].data())
-    const tests = await getDocs(query(collection(db, "tests"), where("hId", "==", hDoc.docs[0].id)));
-    return tests;
+    const d = []
+    const tests = await getDocs(query(collection(db, "testHistory"), where("hId", "==", hId)))
+    await Promise.all(
+      tests.docs.map(async (doc) => {
+        const patient = await getDoc(doc.data().patientId)
+        d.push({ ...doc.data(), PatientName: patient.data().name })
+      })
+    )
+    return d
   }
 
   const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
+    setPage(newPage)
+  }
 
   const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-  
-  return (
-    <div style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    }}>
-      <Paper sx={{ width: '80%', overflow: 'hidden' }} style={{ margin: "1rem 5rem 1rem 5rem" }}>
-        <TableContainer sx={{ maxHeight: 440 }}>
-          <Table stickyHeader aria-label="collapsible table">
-            <TableHead>
-              <TableRow>
-                {columns.map((column) => (
-                  <TableCell
-                    key={column.id}
-                    align={column.align}
-                    style={{ minWidth: column.minWidth }}
-                  >
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => {
-                  return (
-                    <Row key={row.name} row={row} />
-                  );
-                })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Paper>
-    </div>
-  );
-}
+    setRowsPerPage(+event.target.value)
+    setPage(0)
+  }
 
+  if (!loading && !user) return <NotUser />
+  else if (loading || isLoading) return <Loader />
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Paper sx={{ width: "80%", overflow: "hidden" }} style={{ margin: "1rem 5rem 1rem 5rem" }}>
+          <TableContainer sx={{ maxHeight: 440 }}>
+            <Table stickyHeader aria-label="collapsible table">
+              <TableHead>
+                <TableRow>
+                  {columns.map((column) => (
+                    <TableCell key={column.id} align={column.align} style={{ minWidth: column.minWidth }}>
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row,index) => {
+                  return <Row key={index} row={row} />
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={data.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      )}
+    </div>
+  )
+}
