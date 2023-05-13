fefd6fa3c87338dda25ed00a0ba933dc98986ad0
DarthSalad <piyushmishra965@gmail.com>
2023-05-13 21:10:52 +0530

    hosted socket server link

--- web-portal/providers/Socket.provider.js
+++ web-portal/providers/Socket.provider.js
@@ -6,7 +6,10 @@ const SocketContext = React.createContext(null);
 export const useSocket = () => React.useContext(SocketContext);
 
 const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8001"), []);
+  const socket = useMemo(
+		() => io("https://wellnationscoketserver-piyushmishra965.b4a.run/"),
+		[]
+	);
 
   return (
     <SocketContext.Provider value={{ socket }}>
