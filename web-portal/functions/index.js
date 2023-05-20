7f847684920f6ee5d0701b51a0b390c91777f6e5
DarthSalad <piyushmishra965@gmail.com>
2023-05-20 23:10:42 +0530

    updated cloud function

--- web-portal/functions/index.js
+++ web-portal/functions/index.js
@@ -2,36 +2,37 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.notifyAmbulanceStatusChange = functions.firestore
-	.document("ambulance/{ambulanceId}")
+exports.notifyAmbulanceStatusChange = functions
+	.region("asia-south1")
+	.firestore.document("ambulance/{ambulanceId}")
 	.onUpdate((change, context) => {
 		const newValue = change.after.data();
 		const previousValue = change.before.data();
 
 		// Check if the status parameter changed from true to false
 		if (previousValue.status === true && newValue.status === false) {
-			const ambulanceId = context.params.ambulanceId;
+			const vehicleNo = newValue.vechilenumber;
 
 			// Get the recipientToken from the ambulance document
 			const recipientToken = newValue.fcmToken;
 
 			// Send the notification to the recipientToken
 			// Implement your notification logic here
-			sendNotification(recipientToken);
+			sendNotification(recipientToken, vehicleNo);
 		}
 
 		return null;
 	});
 
-function sendNotification(recipientToken) {
+function sendNotification(recipientToken, vehicleNo) {
 	// Implement your notification logic here
 	// Use a third-party service or Firebase Cloud Messaging (FCM) to send the notification
 	// Example: send a push notification using FCM
 
 	const message = {
 		notification: {
-			title: "Ambulance Status Changed",
-			body: "The status of the ambulance has changed.",
+			title: "Patient Booked an Ambulance",
+			body: "A new ambulance booking has been made for the vehicle number: " + vehicleNo,
 		},
 		token: recipientToken,
 	};
