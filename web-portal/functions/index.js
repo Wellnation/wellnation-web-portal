fcab72e1341fcf66ee9cf907ebd3d741c517a3ee
DarthSalad <piyushmishra965@gmail.com>
2023-05-20 23:51:44 +0530

    ambulance function updated

--- web-portal/functions/index.js
+++ web-portal/functions/index.js
@@ -12,34 +12,56 @@ exports.notifyAmbulanceStatusChange = functions
 		// Check if the status parameter changed from true to false
 		if (previousValue.status === true && newValue.status === false) {
 			const vehicleNo = newValue.vechilenumber;
-
-			// Get the recipientToken from the ambulance document
-			const recipientToken = newValue.fcmToken;
+			// Get the fcmTokens from the ambulance document of patient and ambulance
+			const patientToken = newValue.pfcmToken;
+			const ambulanceToken = newValue.fcmToken;
 
 			// Send the notification to the recipientToken
 			// Implement your notification logic here
-			sendNotification(recipientToken, vehicleNo);
+			sendNotification(ambulanceToken, patientToken, vehicleNo);
 		}
 
 		return null;
 	});
 
-function sendNotification(recipientToken, vehicleNo) {
+function sendNotification(recipientToken, patientToken, vehicleNo) {
 	// Implement your notification logic here
 	// Use a third-party service or Firebase Cloud Messaging (FCM) to send the notification
 	// Example: send a push notification using FCM
 
-	const message = {
+	const ambulanceMessage = {
 		notification: {
 			title: "Patient Booked an Ambulance",
-			body: "A new ambulance booking has been made for the vehicle number: " + vehicleNo,
+			body:
+				"A new ambulance booking has been made for the vehicle number: " +
+				vehicleNo,
 		},
 		token: recipientToken,
 	};
 
+	const patientMessage = {
+		notification: {
+			title: "Ambulance Booked",
+			body:
+				"Your ambulance booking has been confirmed for the vehicle number: " +
+				vehicleNo,
+		},
+		token: patientToken,
+	};
+
 	admin
 		.messaging()
-		.send(message)
+		.send(ambulanceMessage)
+		.then((response) => {
+			console.log("Notification sent successfully:", response);
+		})
+		.catch((error) => {
+			console.error("Error sending notification:", error);
+		});
+
+	admin
+		.messaging()
+		.send(patientMessage)
 		.then((response) => {
 			console.log("Notification sent successfully:", response);
 		})
