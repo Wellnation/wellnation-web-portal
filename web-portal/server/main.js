afcf03fc4067a144e0fcc30ddd05b157c2abc382
DarthSalad <piyushmishra965@gmail.com>
2023-06-26 22:33:14 +0530

    server cors updated

--- web-portal/server/main.js
+++ web-portal/server/main.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "https:/wellnation.live");
-	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
 	res.header(
 		"Access-Control-Allow-Headers",
 		"Content-Type, Authorization, Access-Control-Allow-Origin"
