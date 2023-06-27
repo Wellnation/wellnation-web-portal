490121fdf29dad06a548ccc2428489e58ff14b14
DarthSalad <piyushmishra965@gmail.com>
2023-06-27 17:17:24 +0530

    nginx proxy for external http IP socket

--- web-portal/providers/Socket.provider.js
+++ web-portal/providers/Socket.provider.js
@@ -9,15 +9,16 @@ const SocketProvider = (props) => {
   const socket = useMemo(
 		() =>
 			// io("https://wellnation-socket-server.up.railway.app", {
-			io("https://socket.wellnation.live", {
-			// io("http://localhost:8001", {
+			io("http://35.232.243.162", {
+				// io("http://localhost:8001", {
 				withCredentials: true,
 				extraHeaders: {
 					// "Access-Control-Allow-Origin": "http://localhost:3000",
 					"Access-Control-Allow-Origin": "https://wellnation.live",
 					"Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-					"Access-Control-Allow-Headers": "Content-Type, Authorization, Access-Control-Allow-Origin",
-				}
+					"Access-Control-Allow-Headers":
+						"Content-Type, Authorization, Access-Control-Allow-Origin",
+				},
 			}),
 		// () => io("localhost:8001"),
 		[]
